0e1c8afdd6c72462bd4f579f4eb84967dfa36522
Matt Gallagher <mjg1992@hotmail.co.uk>
2025-01-26 21:27:20 +0000

    Make go available in dev environment

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Development environment for Library Book Tracker";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        go
+      ];
+
+      shellHook = ''
+        go version
+      '';
+    };
+  };
+}
